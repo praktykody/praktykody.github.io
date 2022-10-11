e66e53c776e8df67335742962346b2e15f3106a6
apietryga <antek.pietryga@gmail.com>
2022-10-11 23:53:36 +0200

    #2 + refactor

---
+++ docs/js/docifyConfig.js
@@ -0,0 +1,12 @@
+window.$docsify = {
+  name: '',
+  repo: '',
+  loadSidebar: true,
+  subMaxLevel: 2,
+  // executeScript: true,
+  //  basePath: '/docs/',
+  // alias: {
+  //   // '/.*/_sidebar.md': '/_sidebar.md'
+  //   '/GIT/_sidebar.md': '/_sidebar.md'
+  // }
+}
---
+++ docs/js/topNavbar.js
@@ -0,0 +1,74 @@
+// make top navbar
+// window.$docsify.plugins = [].concat((e, o) => {
+//   e.afterEach( (e, o) => {
+//     console.log("TESTING")
+//   })
+// }, window.$docsify.plugins)
+
+window.addEventListener('load', () => {
+  const nav = document.createElement("nav")
+  nav.style.cssText = `
+    position:fixed;
+    top:0;
+    width:100%;
+    height:3rem;
+    z-index:1;
+    display:flex;
+    align-items:center;
+    justify-content:space-between;
+    background-color:var(--codeBackgroundColor);
+  `
+  const logoWithToggler = document.createElement("div")
+  logoWithToggler.style.cssText = `
+    display:flex;
+    align-item:center;
+  ` 
+  const logo = document.createElement('a')
+  logo.innerHTML = '<div>👨‍🏫 TAI Coders</div>'
+  logo.href = '/#'
+  logo.style.cssText = `
+    cursor:pointer;
+    display:inline-block;
+    font-size:1rem;
+    font-weight:bolder;
+    text-decoration:none;
+    color:inherit;
+  `
+  const sidebarToggler = document.querySelector(".sidebar-toggle")
+  sidebarToggler.style.cssText = `
+    position:relative;
+    width:unset;
+    padding:0 1rem;
+  `
+  const searchBar = document.querySelector(".search")
+  searchBar.style.cssText = `
+    flex:1;
+  `
+
+
+  logoWithToggler.append(sidebarToggler)
+  logoWithToggler.append(logo)
+  nav.append(logoWithToggler)
+  nav.append(searchBar)
+
+
+  const lightDarkButton = document.querySelector("#docsify-darklight-theme")
+  if(lightDarkButton){
+
+    lightDarkButton.style.cssText = `
+      position:relative;
+      top:unset;
+      display:flex;
+    `
+    lightDarkButton.addEventListener("click", () => {
+      "light" === t.defaultTheme ? d("dark") : d("light");
+    });
+  
+    nav.append(lightDarkButton)
+  }
+  
+  document.body.prepend(nav)
+  const aside = document.querySelector('.sidebar');
+  aside.style.cssText = `padding-top:3rem;`
+
+})
